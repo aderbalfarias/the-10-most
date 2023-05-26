aa89e59b9175ad58c721f45e85300afeeb9b8114
Aderbal Farias <aderbalfarias@hotmail.com>
2023-05-26 07:43:02 +0100

    UI implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StackOverflowQuestionsComponent } from './components/stack-overflow-questions/stack-overflow-questions.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MillionPipe } from 'src/shared/million.pipe';
+import { DecimalPipe } from '@angular/common';
+import { UnescapePipe } from 'src/shared/unescape.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StackOverflowQuestionsComponent
+    StackOverflowQuestionsComponent,
+    MillionPipe,
+    UnescapePipe
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [DecimalPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/stack-overflow.service.ts
+++ src/app/services/stack-overflow.service.ts
@@ -14,7 +14,7 @@ export class StackOverflowService {
   getMostViewedQuestions(): Observable<Question[]> {
     const url = environment.stackOverflowMostViewedQuestionsUrl;
   
-    return this.http.get<MostViewedStackOverflow>(url)
+    return this.http.get<MostViewedStackOverflow>('https://mocki.io/v1/553c86f2-c2ab-4c62-b7a3-95171088742b')
     .pipe(
       map(response => { return response.items }),
       catchError(error => {
